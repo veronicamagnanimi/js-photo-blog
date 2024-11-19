3db492278bf42465f50c0495a21a60924c33c0ce
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-19 13:23:44 +0100

    feat: milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-
-
 //prelevo elementi dal dom 
 const rowElem = document.querySelector(".row");
 let image = [];
 
+
 //esecuzione logica
 const printPosts = () => {
     image.forEach(curItem => {
-        rowElem.innerHTML += `<div class="col-lg-6 col-md-3 col-12 ptrel">
+        rowElem.innerHTML += `<div class="col-12 col-md-6 col-lg-4 ptrel">
                 <div class="card">
                     <img src="${curItem.thumbnailUrl}">
                     <div class="card-body">
                         <p class="p-style">${curItem.title}</p>
                     </div>
                 </div>
-            </div>`
-    })
+            </div>`;
+    });
 }
 
 //api
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
     console.log(resp.data);  //restituisce l'intera risposta del server
+    image = resp.data;
     printPosts();
 })
 
